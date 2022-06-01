d7625cf88a08622833fb0c83c6fe406ff833d8af
biplabroy819 <biplabroy819@gmail.com>
2022-06-02 00:11:24 +0530

    assignment complitted

---
+++ src/routes/logger/logger.js
@@ -0,0 +1,7 @@
+
+
+const welcome = function() {
+    console.log('Welcome to my application.I am Biplab Roy and a part of FunctionUp Radon cohort')
+}
+
+module.exports.welcome = welcome
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,9 +1,17 @@
 const express = require('express');
-
+const externalModule = require("./logger/logger");
+const externalModule1 = require("./util/helper");
+const externalModule2 = require("./validator/formatter");
 const router = express.Router();
 
+
 router.get('/test-me', function (req, res) {
-    res.send('My first ever api!')
+    // res.send('My first ever api!')
+    externalModule1.a()
+    externalModule1.b()
+    externalModule1.c()
+    externalModule2.d()
+    externalModule.welcome()
 });
 
 module.exports = router;
