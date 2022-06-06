13491f6264191c06f412f50c4e4d12819fe01a04
biplabroy819 <biplabroy819@gmail.com>
2022-06-06 10:47:18 +0530

    assignment/mongo-session1

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const router = express.Router();
 const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
+// const UserController= require("../controllers/userController");
+const res = require('express/lib/response');
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
+// router.get("/test-me", function (req, res) {
+//     res.send("My first ever api!")
+// })
+
+
+
+
+
+
+router.post("/createbook",async function(req, res)  {
+    let data = req.body;
+    let savedData =await UserModel.create(data)
+    res.send({msg:savedData})
 })
 
-router.post("/createUser", UserController.createUser  )
-
-router.get("/getUsersData", UserController.getUsersData)
+router.get("/getbookData", async function(req,res){
+    let allUsers =await UserModel.find()
+    res.send({msg:allUsers})
+})
 
 module.exports = router;
